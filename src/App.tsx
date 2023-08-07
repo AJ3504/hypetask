77987153c10df699248b797fb02e55677f9cfc2f
Lee Jinsol <jins2ol@naver.com>
2023-08-07 19:55:21 +0900

    chore/router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import supabase from "./config/supabaseClient";
+import Router from "./shared/Router";
 
 function App() {
   console.log(supabase);
+
+  return <Router></Router>;
 }
 
 export default App;
