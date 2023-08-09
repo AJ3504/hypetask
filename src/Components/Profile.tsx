507303b628d6a6a6d6c72caa65738413bed50355
eunhee <eunhee9902@gmail.com>
2023-08-10 02:47:27 +0900

    성취도 표시

--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -2,8 +2,18 @@ import React, { useEffect, useState } from "react";
 import supabase from "../config/supabaseClient";
 import { IntroduceContainer } from "./Introduce";
 import { styled } from "styled-components";
+import BarChart from "./AchievementChart";
 
 const Profile: React.FC = () => {
+  const achievementData = [
+    { item: "초심자", achievement: 10 },
+    { item: "노력왕", achievement: 30 },
+    { item: "도전왕", achievement: 50 },
+    { item: "희망왕", achievement: 70 },
+    { item: "열정왕", achievement: 90 },
+
+    { item: "완벽왕", achievement: 100 },
+  ];
   return (
     <MyProfileContainer>
       마이 페이지
@@ -14,6 +24,7 @@ const Profile: React.FC = () => {
       </div>
       <div className="followInfo">팔로잉: 팔로워:</div>
       <div className="achievementRate">목표 달성률</div>
+      <BarChart achievementData={achievementData} />
     </MyProfileContainer>
   );
 };
