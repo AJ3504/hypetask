af1e128c43cb8582e2a2e06c50aca0c158a512a3
baguni99 <131259423+baguni99@users.noreply.github.com>
2023-08-14 11:10:17 +0900

    Update Introduce.tsx

--- src/components/Authentication/Introduce.tsx
+++ src/components/Authentication/Introduce.tsx
@@ -5,10 +5,13 @@ const Introduce: React.FC = () => {
   return (
     <IntroduceContainer>
       <IntroduceTitle>HYPETASK</IntroduceTitle>
-      <div className="introduce-contents">
-        임시 소개글 : 자기개발을 위해 모인 사람들과 함께 일정을 공유하고
-        응원하면서, 각자의 task를 완수하도록 도움을 주는 프로젝트입니다.
-      </div>
+      <IntroduceContents>
+        계획짜러가려면 어디로 가야돼요?
+        <br />
+        뉴칠스의 HYPETASK요. <br />
+        HYPETASK는 일상의 작은 일들부터 큰 프로젝트까지 모두 관리할 수 있는
+        서비스입니다. <br /> HYPETASK와 함께 일상을 체계적으로 관리해보세요! 😊
+      </IntroduceContents>
     </IntroduceContainer>
   );
 };
@@ -17,17 +20,32 @@ export const IntroduceContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  border: 1px solid lightgray;
+  border: 1px solid transparent;
+  background: rgba(255, 255, 250, 0.5);
   flex: 1;
   margin-right: 10px;
   padding: 20px;
   border-radius: 20px;
   height: 400px;
   width: 300px;
+  color: #29425d;
 `;
 export const IntroduceTitle = styled.div`
-  font-size: 24px;
+  font-size: 35px;
   font-weight: bold;
-  margin-bottom: 10px;
+  margin-bottom: 45px;
+  text-align: center;
+  font-style: italic;
+  font-weight: 900;
+  color: #29425d;
+`;
+export const IntroduceContents = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  line-height: 1.5;
+  font-weight: 600;
 `;
 export default Introduce;
